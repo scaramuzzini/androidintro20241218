e22e12b28c343ba4d389426f119d1ad288bf3974
Mario Torres <scaramuzzini@gmail.com>
2024-12-18 18:17:38 -0500

    Formulario Activity

---
+++ app/src/main/java/com/oceanbrasil/androidintrodezembro/FormularioActivity.kt
@@ -0,0 +1,40 @@
+package com.oceanbrasil.androidintrodezembro
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FormularioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_formulario)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btSalvarForm = findViewById<Button>(R.id.btSalvarForm)
+        val etNome = findViewById<EditText>(R.id.etNome)
+        btSalvarForm.setOnClickListener {
+            val nomeDigitado = etNome.text.toString()
+            if (nomeDigitado.isBlank()) {
+                Toast.makeText(applicationContext,
+                    "Você precisa digitar um nome"
+                    , Toast.LENGTH_LONG).show()
+                etNome.error = "Nome obrigatório"
+            } else {
+                Toast.makeText(applicationContext,
+                    nomeDigitado, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+}
